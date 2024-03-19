bf725c3c1664c5005155c4a03008d9743d9739f8
rejdeboer <rick.deboer@live.nl>
2024-03-19 22:32:12 +0100

    fix: use correct CIDR range for aks subnet

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -32,5 +32,5 @@ resource "azurerm_subnet" "aks" {
   name                 = "aks-subnet"
   virtual_network_name = azurerm_virtual_network.network.name
   resource_group_name  = azurerm_resource_group.resource_group.name
-  address_prefixes     = ["10.240.0.0/16"]
+  address_prefixes     = ["10.0.0.0/24"]
 }
