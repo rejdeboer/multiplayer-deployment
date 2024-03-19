711bda26e13217da2b3258982d59e55e4ac4be6b
rejdeboer <rick.deboer@live.nl>
2024-03-19 12:19:32 +0100

    feat: use terraform outputs to define resource group name and cluster name

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,11 @@ resource "local_file" "kubeconfig" {
   filename   = "kubeconfig"
   content    = azurerm_kubernetes_cluster.cluster.kube_config_raw
 }
+
+output "resource_group_name" {
+  value = azurerm_resource_group.resource_group.name
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.cluster.name
+}
