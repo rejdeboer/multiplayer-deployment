e6648619480f698ea818246933a04e057aff9843
rejdeboer <rick.deboer@live.nl>
2024-03-20 17:36:41 +0100

    fix: disable workload id user for now for aks

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -4,7 +4,6 @@ resource "azurerm_kubernetes_cluster" "cluster" {
   resource_group_name = azurerm_resource_group.resource_group.name
   dns_prefix          = "${local.project_name}-cluster"
   sku_tier            = "Free"
-  workload_identity_enabled = true
 
   network_profile {
     network_plugin = "kubenet"
