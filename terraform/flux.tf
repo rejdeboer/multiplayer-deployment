499d958e0691c65088e85e61a8409568cfa40449
rejdeboer <rick.deboer@live.nl>
2024-03-21 20:03:37 +0100

    fix: remove duplicate repository owner

--- terraform/flux.tf
+++ terraform/flux.tf
@@ -6,7 +6,7 @@ provider "flux" {
     cluster_ca_certificate = azurerm_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate
   }
   git = {
-    url = "ssh://git@github.com/${var.github_org}/${var.github_repository}.git"
+    url = "ssh://git@github.com/${var.github_repository}.git"
     ssh = {
       username    = "git"
       private_key = tls_private_key.flux.private_key_pem
