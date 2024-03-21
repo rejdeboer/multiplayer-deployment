455829843ed4dc8a4ea16dcbbb16ad92cac930a1
rejdeboer <rick.deboer@live.nl>
2024-03-21 21:17:47 +0100

    chore: destroy flux bootstrap

--- terraform/flux.tf
+++ terraform/flux.tf
@@ -15,8 +15,8 @@ provider "flux" {
   }
 }
 
-resource "flux_bootstrap_git" "this" {
-  depends_on = [github_repository_deploy_key.this]
-  path       = "cluster"
-}
+# resource "flux_bootstrap_git" "this" {
+#   depends_on = [github_repository_deploy_key.this]
+#   path       = "cluster"
+# }
 
