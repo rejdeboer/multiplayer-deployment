181df9378540020e66977272115688b090da045c
rejdeboer <rick.deboer@live.nl>
2024-03-21 19:54:59 +0100

    chore: change usage of tenant_id in key_vault.tf

--- terraform/key_vault.tf
+++ terraform/key_vault.tf
@@ -2,7 +2,7 @@ resource "azurerm_key_vault" "akv" {
   name                = "${local.project_name}-vault"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
-  tenant_id           = var.azure_tenant_id
+  tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
 
   enabled_for_disk_encryption   = true
@@ -13,7 +13,7 @@ resource "azurerm_key_vault" "akv" {
   access_policy = [
     {
       application_id = ""
-      tenant_id      = var.azure_tenant_id
+      tenant_id      = data.azurerm_client_config.current.tenant_id
       object_id      = data.azurerm_client_config.current.object_id
       secret_permissions = [
         "Get", "List", "Set", "Purge", "Recover", "Delete"
@@ -24,7 +24,7 @@ resource "azurerm_key_vault" "akv" {
     },
     {
       application_id = ""
-      tenant_id      = var.azure_tenant_id
+      tenant_id      = data.azurerm_client_config.current.tenant_id
       object_id      = azurerm_kubernetes_cluster.cluster.key_vault_secrets_provider[0].secret_identity[0].object_id
       secret_permissions = [
         "Get", "List"
