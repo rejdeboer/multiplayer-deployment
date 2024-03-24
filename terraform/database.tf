ea273de1719661db2912eef04743c5e8586c70c1
rejdeboer <rick.deboer@live.nl>
2024-03-24 13:57:50 +0100

    feat: connect to postgres db from nodes

--- terraform/database.tf
+++ terraform/database.tf
@@ -16,7 +16,7 @@ resource "azurerm_postgresql_flexible_server" "this" {
 }
 
 resource "azurerm_postgresql_flexible_server_database" "this" {
-  name      = "${local.project_name}-db"
+  name      = local.project_name
   server_id = azurerm_postgresql_flexible_server.this.id
   charset   = "UTF8"
   collation = "en_US.utf8"
