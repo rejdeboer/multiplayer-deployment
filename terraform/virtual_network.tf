1c47382a8fe58a82191e7eda07921101b1c5afe4
rejdeboer <rick.deboer@live.nl>
2024-03-24 12:44:39 +0100

    fix: remove duplicate declaration of db subnet

--- terraform/virtual_network.tf
+++ terraform/virtual_network.tf
@@ -5,32 +5,3 @@ resource "azurerm_virtual_network" "this" {
   address_space       = ["10.1.0.0/16"]
 }
 
-resource "azurerm_subnet" "db" {
-  name                 = "db-sn"
-  resource_group_name  = azurerm_resource_group.resource_group.name
-  virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = ["10.1.2.0/24"]
-  service_endpoints    = ["Microsoft.Storage"]
-  delegation {
-    name = "fs"
-    service_delegation {
-      name = "Microsoft.DBforPostgreSQL/flexibleServers"
-      actions = [
-        "Microsoft.Network/virtualNetworks/subnets/join/action",
-      ]
-    }
-  }
-}
-
-resource "azurerm_private_dns_zone" "db" {
-  name                = "multiplayer.database.azure.com"
-  resource_group_name = azurerm_resource_group.resource_group.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "db" {
-  name                  = "database.com"
-  private_dns_zone_name = azurerm_private_dns_zone.db.name
-  virtual_network_id    = azurerm_virtual_network.this.id
-  resource_group_name   = azurerm_resource_group.resource_group.name
-  depends_on            = [azurerm_subnet.db]
-}
