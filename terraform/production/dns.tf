682982053b0d7b356364d3a8790882f056e299b4
rejdeboer <rick.deboer@live.nl>
2024-05-30 19:08:42 +0200

    fix: use correct way to index name server

--- terraform/production/dns.tf
+++ terraform/production/dns.tf
@@ -10,31 +10,35 @@ resource "azurerm_dns_zone" "this" {
 resource "cloudflare_record" "name_server_0" {
   zone_id = var.cloudflare_zone_id
   name    = "@"
-  value   = azurerm_dns_zone.this.name_servers[0]
+  value   = azurerm_dns_zone.this.name_servers.0
   type    = "NS"
   ttl     = 86400
+  proxied = false
 }
 
 resource "cloudflare_record" "name_server_1" {
   zone_id = var.cloudflare_zone_id
   name    = "@"
-  value   = azurerm_dns_zone.this.name_servers[1]
+  value   = azurerm_dns_zone.this.name_servers.1
   type    = "NS"
   ttl     = 86400
+  proxied = false
 }
 
 resource "cloudflare_record" "name_server_2" {
   zone_id = var.cloudflare_zone_id
   name    = "@"
-  value   = azurerm_dns_zone.this.name_servers[2]
+  value   = azurerm_dns_zone.this.name_servers.2
   type    = "NS"
   ttl     = 86400
+  proxied = false
 }
 
 resource "cloudflare_record" "name_server_3" {
   zone_id = var.cloudflare_zone_id
   name    = "@"
-  value   = azurerm_dns_zone.this.name_servers[3]
+  value   = azurerm_dns_zone.this.name_servers.3
   type    = "NS"
   ttl     = 86400
+  proxied = false
 }
