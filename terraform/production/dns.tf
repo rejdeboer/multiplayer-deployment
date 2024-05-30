346e7c53525fca846dbd7754539b95433486b3ac
rejdeboer <rick.deboer@live.nl>
2024-05-30 18:55:44 +0200

    feat: configure cloudflare provider

--- terraform/production/dns.tf
+++ terraform/production/dns.tf
@@ -1,4 +1,5 @@
-resource "azurerm_dns_zone" "this" {
-  name                = "rejdeboer.com"
-  resource_group_name = azurerm_resource_group.this.name
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
+
+
