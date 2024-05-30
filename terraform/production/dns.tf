72067a91ca82cea392e8404fc43bad2c636f16a5
rejdeboer <rick.deboer@live.nl>
2024-05-30 21:41:25 +0200

    fix: use tolist operator instead of for_each for dns

--- terraform/production/dns.tf
+++ terraform/production/dns.tf
@@ -7,12 +7,37 @@ resource "azurerm_dns_zone" "this" {
   resource_group_name = azurerm_resource_group.resource_group.name
 }
 
-resource "cloudflare_record" "name_servers" {
-  for_each = azurerm_dns_zone.this.name_servers
-
+resource "cloudflare_record" "name_server_0" {
   zone_id = var.cloudflare_zone_id
   name    = "@"
-  value   = each.value
+  value   = tolist(azurerm_dns_zone.this.name_servers)[0]
+  type    = "NS"
+  ttl     = 86400
+  proxied = false
+}
+
+resource "cloudflare_record" "name_server_1" {
+  zone_id = var.cloudflare_zone_id
+  name    = "@"
+  value   = tolist(azurerm_dns_zone.this.name_servers)[1]
+  type    = "NS"
+  ttl     = 86400
+  proxied = false
+}
+
+resource "cloudflare_record" "name_server_2" {
+  zone_id = var.cloudflare_zone_id
+  name    = "@"
+  value   = tolist(azurerm_dns_zone.this.name_servers)[2]
+  type    = "NS"
+  ttl     = 86400
+  proxied = false
+}
+
+resource "cloudflare_record" "name_server_3" {
+  zone_id = var.cloudflare_zone_id
+  name    = "@"
+  value   = tolist(azurerm_dns_zone.this.name_servers)[3]
   type    = "NS"
   ttl     = 86400
   proxied = false
