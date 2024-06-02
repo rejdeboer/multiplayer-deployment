1bfb14296b4c06216a3b1f7db196e6f7af541683
rejdeboer <rick.deboer@live.nl>
2024-06-02 15:16:21 +0200

    fix: use correct resource group name

--- terraform/production/frontend.tf
+++ terraform/production/frontend.tf
@@ -25,7 +25,7 @@ resource "vercel_deployment" "this" {
 resource "azurerm_dns_cname_record" "www" {
   name                = "www"
   zone_name           = azurerm_dns_zone.this.name
-  resource_group_name = azurerm_resource_group.this.name
+  resource_group_name = azurerm_resource_group.resource_group.name
   ttl                 = 300
   record              = vercel_deployment.this.domains[0]
 }
@@ -33,7 +33,7 @@ resource "azurerm_dns_cname_record" "www" {
 resource "azurerm_dns_cname_record" "frontend" {
   name                = "@"
   zone_name           = azurerm_dns_zone.this.name
-  resource_group_name = azurerm_resource_group.this.name
+  resource_group_name = azurerm_resource_group.resource_group.name
   ttl                 = 300
   record              = vercel_deployment.this.domains[0]
 }
