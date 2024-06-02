6ffd3329d3cc9b7b0aab3360da2d2dcc123818a5
rejdeboer <rick.deboer@live.nl>
2024-06-02 15:14:22 +0200

    fix: example to this

--- terraform/production/frontend.tf
+++ terraform/production/frontend.tf
@@ -24,16 +24,16 @@ resource "vercel_deployment" "this" {
 
 resource "azurerm_dns_cname_record" "www" {
   name                = "www"
-  zone_name           = azurerm_dns_zone.example.name
-  resource_group_name = azurerm_resource_group.example.name
+  zone_name           = azurerm_dns_zone.this.name
+  resource_group_name = azurerm_resource_group.this.name
   ttl                 = 300
   record              = vercel_deployment.this.domains[0]
 }
 
 resource "azurerm_dns_cname_record" "frontend" {
   name                = "@"
-  zone_name           = azurerm_dns_zone.example.name
-  resource_group_name = azurerm_resource_group.example.name
+  zone_name           = azurerm_dns_zone.this.name
+  resource_group_name = azurerm_resource_group.this.name
   ttl                 = 300
   record              = vercel_deployment.this.domains[0]
 }
